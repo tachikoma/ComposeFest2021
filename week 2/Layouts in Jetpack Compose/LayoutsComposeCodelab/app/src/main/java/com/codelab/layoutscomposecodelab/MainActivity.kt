0ee125bbd941c7024fe38e4dfe87739c34af9c34
Durk-Jae Yun <durkjae.yun@gmail.com>
2021-11-07 17:00:37 +0900

    Layouts in Jatpack Compose Codelab

---
+++ week 2/Layouts in Jetpack Compose/LayoutsComposeCodelab/app/src/main/java/com/codelab/layoutscomposecodelab/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.codelab.layoutscomposecodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.layoutscomposecodelab.ui.theme.LayoutsComposeCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsComposeCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutsComposeCodelabTheme {
+        Greeting("Android")
+    }
+}
